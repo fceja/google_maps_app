adde62f1178675c63cef66ee6f4fa4333608746d
FC <cisco_ceja@icloud.com>
2023-08-15 08:24:05 -0700

    add base work for app

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return <div>This is App div</div>;
+};
+
+export default App;
---
+++ src/components/Root.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import App from "../components/App";
+
+const Root: React.FC = () => {
+  return <App />;
+};
+
+export default Root;
